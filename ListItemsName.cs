8f49d423113fda8b648d9ccf7dfc419fca9033c0
Akash santhosh <akash777kk@gmail.com>
2023-10-03 22:17:56 +0530

    Add project files.

---
+++ ListItemsName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListApp
+{
+    internal class ListItemsName
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public int Unit { get; set; }
+
+        public double Amount { get; set; }
+
+        public double Balance { get { return Unit * Amount; } }
+
+        public ListItemsName(int id,string name,int unit,double amount) {
+        
+            Id = id;
+            Name = name;
+            Unit = unit;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{Amount}";
+        }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ListApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ListItemsName> list = new List<ListItemsName>()
+            { new ListItemsName(101,"milk",2,20),
+            new ListItemsName(102,"Darymilk",4,50),
+            new ListItemsName(103,"book",2,100),
+            };
+            PrintItems(list);
+
+
+            ListItemsName listItemsNam2 = new ListItemsName(1, "akash", 4, 200);
+            Console.WriteLine(listItemsNam2);//to string used
+        }
+       public static void PrintItems(List<ListItemsName> list)
+        {
+            double totalCost = 0;
+            foreach (var item in list)
+            {
+                Console.WriteLine("asd:"+item);
+                totalCost += item.Balance;
+
+            }
+            Console.WriteLine(totalCost);
+        }
+
+        
+    }
+}
